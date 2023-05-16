450584b51a9290c55b8d09a5192354c56ef1feee
Andrew Topping <andrew@thoughtcollective.com>
2023-05-16 15:56:52 +0100

    Update Controller Handling
    Update to handle how new versions of Laravel deal with controllers.

--- routes/cp.php
+++ routes/cp.php
@@ -1,7 +1,10 @@
 <?php
 
+use NineteenSquared\Instagram\Http\Controllers\InstagramLoginController;
+use NineteenSquared\Instagram\Http\Controllers\InstagramLogoutController;
+
 Route::prefix('nineteen-ig/')->name('nineteen-ig.')->group(function () {
-    Route::get('/', 'InstagramLoginController@index')->name('index');
-    Route::get('/logout', 'InstagramLogoutController')->name('logout');
-    Route::get('/auth', 'InstagramLoginController@callback')->name('callback');
+    Route::get('/', [InstagramLoginController::class, 'index'])->name('index');
+    Route::get('/logout', InstagramLogoutController::class)->name('logout');
+    Route::get('/auth', [InstagramLoginController::class, 'callback'])->name('callback');
 });
